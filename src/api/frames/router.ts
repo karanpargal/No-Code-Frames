ee60900b7f9781157d886af3f9815d5e6f15259d
Srivastava57Harsh <srivastava57harsh@gmail.com>
2024-03-24 16:14:34 +0530

    :sparkles: Add weirdly coded render all frames
    add weirdly coded render all frames

--- src/api/frames/router.ts
+++ src/api/frames/router.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import LoggerInstance from '../../loaders/logger';
-import { createFrame, renderFrame } from './controller';
-import { createFrameValidator, renderFrameValidator } from './validator';
+import { createFrame, renderUserFrame, renderFrame } from './controller';
+import { createFrameValidator, renderFrameValidator, renderUserFramesValidator } from './validator';
 const framesRouter = Router();
 
 async function handleCreateFrame(req: Request, res: Response) {
@@ -47,8 +47,31 @@ async function handleRenderFrame(req: Request, res: Response) {
   }
 }
 
+async function handleRenderUserFrames(req: Request, res: Response) {
+  try {
+    const result = await renderUserFrame(req.body.walletAddress);
+    if (result.bool) {
+      res.status(200).send({
+        data: result.data,
+      });
+    } else {
+      throw {
+        status: 400,
+        message: result.message,
+      };
+    }
+  } catch (e) {
+    LoggerInstance.error(e);
+    res.status(e.status || 500).json({
+      message: e.message || 'Request Failed',
+    });
+  }
+}
+
 framesRouter.post('/createFrame', createFrameValidator, handleCreateFrame);
 
 framesRouter.post('/renderFrame', renderFrameValidator, handleRenderFrame);
+
+framesRouter.post('/renderUserFrames', renderUserFramesValidator, handleRenderUserFrames);
 
 export default framesRouter;
