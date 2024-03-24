2361236bae3ec73d6605f0c1170c5ec81a8d2095
Karan <karanpargal007@gmail.com>
2024-03-24 18:49:43 +0530

    view frames ui

--- client/app/dashboard/myFrames/page.tsx
+++ client/app/dashboard/myFrames/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { useAccount } from 'wagmi';
 
 export default function MyFrames() {
   const [frames, setFrames] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const { address } = useAccount();
 
   console.log(address);
 
   useEffect(() => {
+    setLoading(true);
     const fetchFrames = async () => {
       try {
         const response = await fetch('http://localhost:3000/api/frames/renderUserFrames', {
@@ -29,8 +32,11 @@ export default function MyFrames() {
         }
 
         console.log(data.data);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching frames:', error);
+        toast.error('Error fetching frames');
+        setLoading(false);
       }
     };
 
@@ -38,7 +44,7 @@ export default function MyFrames() {
   }, [address]);
 
   return (
-    <div className="bg-gradient-to-b from-gray-900 via-pink-700 to-white h-screen p-8">
+    <div className="bg-white h-screen p-8 text-black">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6 mb-4 cursor-pointer"
@@ -50,22 +56,97 @@ export default function MyFrames() {
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
 
-      <h1 className="text-white text-center text-4xl mb-8">My Frames</h1>
-      <div className="grid grid-cols-3 gap-4">
-        {frames.map((frame: any) => (
-          <div key={frame._id} className="bg-white rounded-lg shadow-md p-4 text-black">
-            <h2 className="text-xl font-semibold mb-2">Frame</h2>
-            <p className="mb-4">{frame.frame}</p>
-            <a
-              href={`http://localhost:3010/?url=http://localhost:3000/api/frames/fetchFrame/${frame._id}`}
-              target="_blank"
-              className="bg-blue-500 px-4 py-2 rounded-md text-white mt-2"
-            >
-              View Frame
-            </a>
-          </div>
-        ))}
-      </div>
+      <h1 className="text-black text-center text-4xl mb-8">My Frames</h1>
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
+        </div>
+      ) : (
+        <>
+          {frames.length > 0 ? (
+            <div className="grid grid-cols-3 gap-4">
+              {frames.map((frame: any) => (
+                <div key={frame._id} className="bg-white rounded-sm shadow-md p-4 text-black border-black border-2">
+                  <h2 className="text-xl font-semibold mb-2">Frame</h2>
+                  <p className="">
+                    Frame ID:{' '}
+                    <span
+                      className="flex gap-2 font-bold items-center cursor-pointer"
+                      onClick={() => {
+                        navigator.clipboard.writeText(frame._id);
+                        toast.success('Frame ID copied to clipboard');
+                      }}
+                    >
+                      {frame._id}
+                      <svg
+                        fill="#000000"
+                        height="16px"
+                        width="16px"
+                        version="1.1"
+                        id="Layer_1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlnsXlink="http://www.w3.org/1999/xlink"
+                        viewBox="0 0 64 64"
+                        enable-background="new 0 0 64 64"
+                        xmlSpace="preserve"
+                      >
+                        <g id="Text-files">
+                          <path
+                            d="M53.9791489,9.1429005H50.010849c-0.0826988,0-0.1562004,0.0283995-0.2331009,0.0469999V5.0228
+                       C49.7777481,2.253,47.4731483,0,44.6398468,0h-34.422596C7.3839517,0,5.0793519,2.253,5.0793519,5.0228v46.8432999
+                       c0,2.7697983,2.3045998,5.0228004,5.1378999,5.0228004h6.0367002v2.2678986C16.253952,61.8274002,18.4702511,64,21.1954517,64
+                       h32.783699c2.7252007,0,4.9414978-2.1725998,4.9414978-4.8432007V13.9861002
+                       C58.9206467,11.3155003,56.7043495,9.1429005,53.9791489,9.1429005z M7.1110516,51.8661003V5.0228
+                       c0-1.6487999,1.3938999-2.9909999,3.1062002-2.9909999h34.422596c1.7123032,0,3.1062012,1.3422,3.1062012,2.9909999v46.8432999
+                       c0,1.6487999-1.393898,2.9911003-3.1062012,2.9911003h-34.422596C8.5049515,54.8572006,7.1110516,53.5149002,7.1110516,51.8661003z
+                        M56.8888474,59.1567993c0,1.550602-1.3055,2.8115005-2.9096985,2.8115005h-32.783699
+                       c-1.6042004,0-2.9097996-1.2608986-2.9097996-2.8115005v-2.2678986h26.3541946
+                       c2.8333015,0,5.1379013-2.2530022,5.1379013-5.0228004V11.1275997c0.0769005,0.0186005,0.1504021,0.0469999,0.2331009,0.0469999
+                       h3.9682999c1.6041985,0,2.9096985,1.2609005,2.9096985,2.8115005V59.1567993z"
+                          />
+                          <path
+                            d="M38.6031494,13.2063999H16.253952c-0.5615005,0-1.0159006,0.4542999-1.0159006,1.0158005
+                       c0,0.5615997,0.4544001,1.0158997,1.0159006,1.0158997h22.3491974c0.5615005,0,1.0158997-0.4542999,1.0158997-1.0158997
+                       C39.6190491,13.6606998,39.16465,13.2063999,38.6031494,13.2063999z"
+                          />
+                          <path
+                            d="M38.6031494,21.3334007H16.253952c-0.5615005,0-1.0159006,0.4542999-1.0159006,1.0157986
+                       c0,0.5615005,0.4544001,1.0159016,1.0159006,1.0159016h22.3491974c0.5615005,0,1.0158997-0.454401,1.0158997-1.0159016
+                       C39.6190491,21.7877007,39.16465,21.3334007,38.6031494,21.3334007z"
+                          />
+                          <path
+                            d="M38.6031494,29.4603004H16.253952c-0.5615005,0-1.0159006,0.4543991-1.0159006,1.0158997
+                       s0.4544001,1.0158997,1.0159006,1.0158997h22.3491974c0.5615005,0,1.0158997-0.4543991,1.0158997-1.0158997
+                       S39.16465,29.4603004,38.6031494,29.4603004z"
+                          />
+                          <path
+                            d="M28.4444485,37.5872993H16.253952c-0.5615005,0-1.0159006,0.4543991-1.0159006,1.0158997
+                       s0.4544001,1.0158997,1.0159006,1.0158997h12.1904964c0.5615025,0,1.0158005-0.4543991,1.0158005-1.0158997
+                       S29.0059509,37.5872993,28.4444485,37.5872993z"
+                          />
+                        </g>
+                      </svg>
+                    </span>
+                  </p>
+                  <p className="my-4 overflow-hidden mb-6">{frame.frame}</p>
+
+                  <a
+                    href={`http://localhost:3010/?url=http://localhost:3000/api/frames/fetchFrame/${frame._id}`}
+                    target="_blank"
+                    className="relative inline-block px-4 py-2 font-medium group"
+                  >
+                    <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                    <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                    <span className="relative text-black group-hover:text-white">View Frame</span>
+                  </a>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className="text-center">No frames found</p>
+          )}
+        </>
+      )}
     </div>
   );
 }
